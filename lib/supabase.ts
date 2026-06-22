9096e2936c7d3d24c2a9882a4f50e08eb2fecba8
Wil Edward Munoz <wiledward@Wils-MacBook-Pro.local>
2026-06-22 16:37:02 -0500

    Lunes 22 03

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,17 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl) {
-  throw new Error("NEXT_PUBLIC_SUPABASE_URL no definida");
-}
-
-if (!supabaseAnonKey) {
-  throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY no definida");
-}
-
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
